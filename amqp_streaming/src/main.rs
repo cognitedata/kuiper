c0dafb14e61421c07977876dccb58acc6298081c
Einar Omang <einar.omang@cognite.com>
2023-02-28 08:42:07 +0100

    Amqp

--- amqp_streaming/src/main.rs
+++ amqp_streaming/src/main.rs
@@ -1,5 +1,3 @@
-use std::env;
-
 use anyhow::anyhow;
 use anyhow::Result;
 use fe2o3_amqp::types::messaging::Body;
@@ -11,13 +9,15 @@ use fe2o3_amqp::{
 };
 use fe2o3_amqp_management::{operations::ReadRequest, MgmtClient};
 use json_transform::Program;
+use rumqttc::v5::MqttOptions;
 use serde_json::json;
+use std::env;
 
 #[tokio::main]
 async fn main() {
     let hostname = env::var("AMQP_HOST").unwrap();
     let sa_key_name = env::var("AMQP_KEY_NAME").unwrap();
-    let sa_key_value = env::var("AMQP_KEY").unwrap();
+    let sa_key_value = env::var("AMQP_KEY_VALUE").unwrap();
     let event_hub_name = env::var("EVENT_HUB_NAME").unwrap();
 
     let url = format!("amqps://{hostname}");
@@ -43,6 +43,7 @@ async fn main() {
     }
 
     let partition = &partitions[0];
+    // let partition = 0;
     let partition_address =
         format!("{event_hub_name}/ConsumerGroups/$default/Partitions/{partition}");
 
@@ -56,6 +57,49 @@ async fn main() {
     .await
     .unwrap();
 
+    // stream_cdf(&mut receiver).await;
+    stream_mqtt(&mut receiver).await;
+
+    receiver.close().await.unwrap();
+    session.end().await.unwrap();
+    connection.close().await.unwrap();
+}
+
+async fn stream_mqtt(receiver: &mut Receiver) {
+    let options = MqttOptions::new("amqp_pub", "localhost", 1883);
+
+    let (client, mut event_loop) = rumqttc::v5::AsyncClient::new(options, 1000);
+
+    tokio::spawn(async move {
+        loop {
+            event_loop.poll().await.unwrap();
+        }
+    });
+
+    let mut idx = 0;
+    loop {
+        println!("Begin waiting for amqp delivery");
+        let delivery = receiver.recv::<Body<Value>>().await.unwrap();
+        println!("Received amqp batch");
+        for dt in delivery.body().try_as_data().unwrap() {
+            client
+                .publish(
+                    "example/message",
+                    rumqttc::v5::mqttbytes::QoS::AtLeastOnce,
+                    false,
+                    dt.clone().into_vec(),
+                )
+                .await
+                .unwrap();
+            println!("Received message {}", idx);
+            idx += 1;
+        }
+        println!("");
+        receiver.accept(&delivery).await.unwrap();
+    }
+}
+
+async fn stream_cdf(receiver: &mut Receiver) {
     let transform = Program::compile(
         serde_json::from_value(json!([{
             "id": "get_items",
@@ -93,21 +137,20 @@ async fn main() {
         println!("");
         receiver.accept(&delivery).await.unwrap();
     }
-
-    receiver.close().await.unwrap();
-    session.end().await.unwrap();
-    connection.close().await.unwrap();
 }
 
 pub async fn get_event_hub_partitions(
     connection: &mut ConnectionHandle<()>,
     event_hub_name: &str,
 ) -> Result<Vec<String>> {
-    let mut session = Session::begin(connection).await?;
-    let mut mgmt_client = MgmtClient::attach(&mut session, "mgmt_client_node").await?;
+    let mut session = Session::begin(connection).await.unwrap();
+    let mut mgmt_client = MgmtClient::attach(&mut session, "mgmt_client_node")
+        .await
+        .unwrap();
 
+    println!("Evhub: {}", event_hub_name);
     let request = ReadRequest::name(event_hub_name, "com.microsoft:eventhub", None);
-    let mut response = mgmt_client.call(request).await?;
+    let mut response = mgmt_client.call(request).await.unwrap();
 
     mgmt_client.close().await?;
     session.end().await?;
