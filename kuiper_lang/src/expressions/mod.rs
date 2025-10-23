85b30ac93f0975fbd37bbb572588452eabb6b2ac
Einar Omang <einar.omang@cognite.com>
2025-10-23 08:20:14 +0200

    Add builder type for running the expression
    We're getting some minor combinatorial explosion now with the new input
    type. Instead of creating even more "run" methods on `ExpressionType`,
    add a builder that can be used to provide any combination of inputs and
    outputs.

--- kuiper_lang/src/expressions/mod.rs
+++ kuiper_lang/src/expressions/mod.rs
@@ -9,11 +9,14 @@ mod numbers;
 mod object;
 mod operator;
 mod result;
+mod run_builder;
 mod selector;
 mod source;
 mod transform_error;
 
 pub use array::{ArrayElement, ArrayExpression};
+#[cfg(feature = "completions")]
+pub use base::Completions;
 pub use base::{
     get_function_expression, Constant, Expression, ExpressionExecutionState, ExpressionMeta,
     ExpressionType,
@@ -25,6 +28,7 @@ pub use macro_call::MacroCallExpression;
 pub use object::{ObjectElement, ObjectExpression};
 pub use operator::{OpExpression, Operator, UnaryOpExpression, UnaryOperator};
 pub use result::*;
+pub use run_builder::ExpressionRunBuilder;
 pub use selector::{SelectorElement, SelectorExpression, SourceElement};
 pub use source::SourceData;
 pub use transform_error::{TransformError, TransformErrorData};
