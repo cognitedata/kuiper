faa18a94b4d0a1539f0f44e703527992a5b03c2d
Einar Omang <einar.omang@cognite.com>
2026-02-10 09:05:54 +0100

    Type resolution impls for array functions

--- kuiper_lang/src/expressions/functions/arrays.rs
+++ kuiper_lang/src/expressions/functions/arrays.rs
@@ -3,6 +3,7 @@ use serde_json::{Number, Value};
 
 use crate::{
     expressions::{numbers::JsonNumber, Expression, ResolveResult},
+    types::{Array, Type, TypeError},
     TransformError,
 };
 
@@ -30,6 +31,18 @@ impl<'a: 'c, 'c> Expression<'a, 'c> for LengthFunction {
         };
 
         Ok(ResolveResult::Owned(Value::Number(Number::from(len))))
+    }
+
+    fn resolve_types(
+        &'a self,
+        state: &mut crate::types::TypeExecutionState<'c, '_>,
+    ) -> Result<Type, TypeError> {
+        let source = self.args[0].resolve_types(state)?;
+        source.assert_assignable_to(
+            &Type::Union(vec![Type::String, Type::any_array(), Type::any_object()]),
+            &self.span,
+        )?;
+        Ok(Type::Integer)
     }
 }
 
@@ -77,6 +90,61 @@ impl<'a: 'c, 'c> Expression<'a, 'c> for ChunkFunction {
         }
         Ok(ResolveResult::Owned(Value::Array(res)))
     }
+
+    fn resolve_types(
+        &'a self,
+        state: &mut crate::types::TypeExecutionState<'c, '_>,
+    ) -> Result<Type, TypeError> {
+        let source = self.args[0].resolve_types(state)?;
+        let chunk_size = self.args[1].resolve_types(state)?;
+        chunk_size.assert_assignable_to(&Type::Integer, &self.span)?;
+        let mut source_arr = source.try_as_array(&self.span)?;
+        // If the chunk size is a constant, we can be more precise about the return type.
+        // This is likely pretty common, a dynamic chunk size would be more unusual.
+        if let Type::Constant(Value::Number(n)) = chunk_size.clone() {
+            let Some(n) = n.as_u64() else {
+                return Err(TypeError::ExpectedType(
+                    Box::new(Type::Integer),
+                    Box::new(chunk_size),
+                    self.span.clone(),
+                ));
+            };
+            let n = n as usize;
+            let chunks = source_arr.elements.chunks(n);
+            let mut res_arr = Vec::new();
+            for chunk in chunks {
+                if chunk.len() == n {
+                    res_arr.push(Type::Array(Array {
+                        elements: chunk.to_owned(),
+                        end_dynamic: None,
+                    }));
+                } else {
+                    res_arr.push(Type::Array(Array {
+                        elements: chunk.to_owned(),
+                        end_dynamic: source_arr.end_dynamic.take(),
+                    }));
+                }
+            }
+            if let Some(end_dynamic) = source_arr.end_dynamic.take() {
+                res_arr.push(Type::Array(Array {
+                    elements: vec![],
+                    end_dynamic: Some(end_dynamic),
+                }));
+            }
+            Ok(Type::Array(Array {
+                elements: res_arr,
+                end_dynamic: None,
+            }))
+        } else {
+            Ok(Type::Array(Array {
+                end_dynamic: Some(Box::new(Type::Array(Array {
+                    elements: Vec::new(),
+                    end_dynamic: Some(Box::new(source_arr.element_union())),
+                }))),
+                elements: Vec::new(),
+            }))
+        }
+    }
 }
 
 function_def!(TailFunction, "tail", 1, Some(2));
@@ -122,6 +190,63 @@ impl<'a: 'c, 'c> Expression<'a, 'c> for TailFunction {
                 Ok(ResolveResult::Owned(Value::Array(
                     arr[start..end].to_owned(),
                 )))
+            }
+        }
+    }
+
+    fn resolve_types(
+        &'a self,
+        state: &mut crate::types::TypeExecutionState<'c, '_>,
+    ) -> Result<Type, TypeError> {
+        let source = self.args[0].resolve_types(state)?;
+        let source_arr = source.try_as_array(&self.span)?;
+
+        match self.args.get(1) {
+            None => Ok(source_arr.index_from_end(0).unwrap_or_else(Type::null)),
+            Some(exp) => {
+                let number = exp.resolve_types(state)?;
+                let key = number.clone();
+                number.assert_assignable_to(&Type::Integer, &self.span)?;
+                if let Type::Constant(Value::Number(n)) = number {
+                    let Some(n) = n.as_u64() else {
+                        return Err(TypeError::ExpectedType(
+                            Box::new(Type::Integer),
+                            Box::new(key),
+                            self.span.clone(),
+                        ));
+                    };
+                    if n == 0 {
+                        Ok(Type::null())
+                    } else if n == 1 {
+                        Ok(source_arr.index_from_end(0).unwrap_or_else(Type::null))
+                    } else if let Some(end_dynamic) = source_arr.end_dynamic {
+                        Ok(Type::Array(Array {
+                            end_dynamic: Some(end_dynamic),
+                            elements: vec![],
+                        }))
+                    } else {
+                        let start = source_arr.elements.len().saturating_sub(n as usize);
+                        let res = source_arr.elements[start..].to_owned();
+
+                        Ok(Type::Array(Array {
+                            elements: res,
+                            end_dynamic: None,
+                        }))
+                    }
+                } else {
+                    let mut res = Type::Union(Vec::new());
+                    // If the value is 0
+                    res = res.union_with(Type::null());
+                    // If the value is 1
+                    res = res.union_with(source_arr.index_from_end(0).unwrap_or_else(Type::null));
+                    // If the value is greater than 1. We don't really want to return a combinatorial explosion
+                    // of possible sequences.
+                    res = res.union_with(Type::Array(Array {
+                        elements: vec![],
+                        end_dynamic: Some(Box::new(source_arr.element_union())),
+                    }));
+                    Ok(res)
+                }
             }
         }
     }
@@ -174,6 +299,33 @@ impl<'a: 'c, 'c> Expression<'a, 'c> for SliceFunction {
                 inp_array[start..].iter().cloned().collect_vec(),
             )))
         }
+    }
+
+    fn resolve_types(
+        &'a self,
+        state: &mut crate::types::TypeExecutionState<'c, '_>,
+    ) -> Result<Type, TypeError> {
+        let inp_value = self.args[0].resolve_types(state)?;
+        let inp_array = inp_value.try_as_array(&self.span)?;
+
+        let start = self.args[1].resolve_types(state)?;
+        start.assert_assignable_to(&Type::Integer, &self.span)?;
+
+        let end = self
+            .args
+            .get(2)
+            .map(|c| c.resolve_types(state))
+            .transpose()?;
+        if let Some(end) = end {
+            end.assert_assignable_to(&Type::Integer, &self.span)?;
+        }
+
+        // Technically we could check for constant slicing here, TODO. There's reasonably high value to that,
+        // but it's a lot of work.
+        Ok(Type::Array(Array {
+            elements: vec![],
+            end_dynamic: Some(Box::new(inp_array.element_union())),
+        }))
     }
 }
 
@@ -235,6 +387,26 @@ impl<'a: 'c, 'c> Expression<'a, 'c> for SumFunction {
             },
         )?))
     }
+
+    fn resolve_types(
+        &'a self,
+        state: &mut crate::types::TypeExecutionState<'c, '_>,
+    ) -> Result<Type, TypeError> {
+        let arr = self.args[0].resolve_types(state)?;
+        let arr = arr.try_as_array(&self.span)?;
+
+        let mut return_type = Type::Integer;
+        for it in arr.all_elements() {
+            let ty = it;
+            if ty.is_float() && return_type.is_integer() {
+                return_type = Type::Float;
+            } else if !ty.is_integer() {
+                ty.assert_assignable_to(&Type::number(), &self.span)?;
+                return_type = Type::number();
+            }
+        }
+        Ok(return_type)
+    }
 }
 
 function_def!(ContainsFunction, "contains", 2);
@@ -265,11 +437,26 @@ impl<'a: 'c, 'c> Expression<'a, 'c> for ContainsFunction {
             }
             _ => Err(TransformError::new_incorrect_type(
                 "contains",
-                "array",
+                "array or string",
                 TransformError::value_desc(&raw_list),
                 &self.span,
             )),
         }
+    }
+
+    fn resolve_types(
+        &'a self,
+        state: &mut crate::types::TypeExecutionState<'c, '_>,
+    ) -> Result<Type, TypeError> {
+        let arr = self.args[0].resolve_types(state)?;
+        let check = self.args[1].resolve_types(state)?;
+        arr.assert_assignable_to(&Type::any_array().union_with(Type::String), &self.span)?;
+
+        if matches!(arr, Type::String) {
+            check.assert_assignable_to(&Type::String, &self.span)?;
+        }
+
+        Ok(Type::Boolean)
     }
 }
 
@@ -277,7 +464,10 @@ impl<'a: 'c, 'c> Expression<'a, 'c> for ContainsFunction {
 mod tests {
     use serde_json::Value;
 
-    use crate::compile_expression;
+    use crate::{
+        compile_expression,
+        types::{Array, Type},
+    };
 
     #[test]
     pub fn test_length() {
@@ -424,5 +614,251 @@ mod tests {
         assert_eq!(res.get("t7").unwrap().as_bool().unwrap(), true);
         assert_eq!(res.get("t8").unwrap().as_bool().unwrap(), false);
         assert_eq!(res.get("t9").unwrap().as_bool().unwrap(), true);
+    }
+
+    #[test]
+    pub fn test_length_types() {
+        let expr = compile_expression("input.length()", &["input"]).unwrap();
+        let ty = expr.run_types([Type::String]).unwrap();
+        assert_eq!(Type::Integer, ty);
+        let ty = expr
+            .run_types([Type::array_of_type(Type::Integer)])
+            .unwrap();
+        assert_eq!(Type::Integer, ty);
+        let ty = expr
+            .run_types([Type::object_of_type(Type::String)])
+            .unwrap();
+        assert_eq!(Type::Integer, ty);
+        assert_eq!(Type::Integer, expr.run_types([Type::Any]).unwrap());
+
+        let err = expr.run_types([Type::Integer]).unwrap_err();
+        assert_eq!(
+            "Expected Union<String, [...Any], {...: Any}> but got Integer".to_string(),
+            err.to_string()
+        );
+    }
+
+    #[test]
+    pub fn test_chunk_types() {
+        let expr = compile_expression("input.chunk(input2)", &["input", "input2"]).unwrap();
+        // Unknown chunk size, unknown array type
+        let ty = expr
+            .run_types([Type::array_of_type(Type::Integer), Type::Integer])
+            .unwrap();
+        assert_eq!(
+            Type::Array(crate::types::Array {
+                end_dynamic: Some(Box::new(Type::array_of_type(Type::Integer))),
+                elements: Vec::new(),
+            }),
+            ty
+        );
+
+        // Unknown chunk size, known array type
+        let ty = expr
+            .run_types([
+                Type::Array(Array {
+                    elements: vec![
+                        Type::from_const(1),
+                        Type::from_const(2),
+                        Type::from_const(3),
+                    ],
+                    end_dynamic: None,
+                }),
+                Type::Integer,
+            ])
+            .unwrap();
+        assert_eq!(
+            Type::Array(Array {
+                end_dynamic: Some(Box::new(Type::Array(Array {
+                    elements: Vec::new(),
+                    end_dynamic: Some(Box::new(
+                        Type::from_const(1)
+                            .union_with(Type::from_const(2))
+                            .union_with(Type::from_const(3))
+                    )),
+                }))),
+                elements: Vec::new(),
+            }),
+            ty
+        );
+
+        // Known chunk size, known array type
+        let ty = expr
+            .run_types([
+                Type::Array(Array {
+                    elements: vec![
+                        Type::from_const(1),
+                        Type::from_const(2),
+                        Type::from_const(3),
+                    ],
+                    end_dynamic: None,
+                }),
+                Type::from_const(2),
+            ])
+            .unwrap();
+        assert_eq!(
+            Type::Array(Array {
+                end_dynamic: None,
+                elements: vec![
+                    Type::Array(Array {
+                        elements: vec![Type::from_const(1), Type::from_const(2)],
+                        end_dynamic: None,
+                    }),
+                    Type::Array(Array {
+                        elements: vec![Type::from_const(3)],
+                        end_dynamic: None,
+                    }),
+                ],
+            }),
+            ty
+        );
+
+        // Bad input type
+        let err = expr.run_types([Type::Integer, Type::Integer]).unwrap_err();
+        assert_eq!(
+            "Expected [...Any] but got Integer".to_string(),
+            err.to_string()
+        );
+    }
+
+    #[test]
+    fn test_tail_types() {
+        let expr = compile_expression("input.tail(input2)", &["input", "input2"]).unwrap();
+        // Unknown tail size, unknown array type
+        let ty = expr
+            .run_types([Type::array_of_type(Type::Integer), Type::Integer])
+            .unwrap();
+        assert_eq!(
+            Type::Union(vec![
+                Type::Integer,
+                Type::null(),
+                Type::Array(Array {
+                    elements: Vec::new(),
+                    end_dynamic: Some(Box::new(Type::Integer)),
+                })
+            ]),
+            ty
+        );
+
+        // Known tail size, known array type
+        let ty = expr
+            .run_types([
+                Type::Array(Array {
+                    elements: vec![
+                        Type::from_const(1),
+                        Type::from_const(2),
+                        Type::from_const(3),
+                    ],
+                    end_dynamic: None,
+                }),
+                Type::from_const(2),
+            ])
+            .unwrap();
+        assert_eq!(
+            Type::Array(Array {
+                elements: vec![Type::from_const(2), Type::from_const(3)],
+                end_dynamic: None,
+            }),
+            ty
+        );
+
+        // Unknown tail size, known array type
+        let ty = expr
+            .run_types([
+                Type::Array(Array {
+                    elements: vec![
+                        Type::from_const(1),
+                        Type::from_const(2),
+                        Type::from_const(3),
+                    ],
+                    end_dynamic: None,
+                }),
+                Type::Integer,
+            ])
+            .unwrap();
+        assert_eq!(
+            Type::Union(vec![
+                Type::null(),
+                Type::from_const(3),
+                Type::Array(Array {
+                    elements: Vec::new(),
+                    end_dynamic: Some(Box::new(
+                        Type::from_const(1)
+                            .union_with(Type::from_const(2))
+                            .union_with(Type::from_const(3))
+                    )),
+                })
+            ]),
+            ty
+        );
+        // Bad input type
+        let err = expr.run_types([Type::Integer, Type::Integer]).unwrap_err();
+        assert_eq!(
+            "Expected [...Any] but got Integer".to_string(),
+            err.to_string()
+        );
+    }
+
+    #[test]
+    fn test_slice_types() {
+        let expr = compile_expression(
+            "input.slice(input2, input3)",
+            &["input", "input2", "input3"],
+        )
+        .unwrap();
+        let ty = expr
+            .run_types([
+                Type::array_of_type(Type::Integer),
+                Type::Integer,
+                Type::Integer,
+            ])
+            .unwrap();
+        assert_eq!(
+            Type::Array(Array {
+                elements: Vec::new(),
+                end_dynamic: Some(Box::new(Type::Integer)),
+            }),
+            ty
+        );
+    }
+
+    #[test]
+    fn test_sum_types() {
+        let expr = compile_expression("input.sum()", &["input"]).unwrap();
+        let ty = expr
+            .run_types([Type::array_of_type(Type::Integer)])
+            .unwrap();
+        assert_eq!(Type::Integer, ty);
+
+        let ty = expr.run_types([Type::array_of_type(Type::Float)]).unwrap();
+        assert_eq!(Type::Float, ty);
+
+        let ty = expr
+            .run_types([Type::array_of_type(Type::number())])
+            .unwrap();
+        assert_eq!(Type::number(), ty);
+
+        let err = expr
+            .run_types([Type::array_of_type(Type::String)])
+            .unwrap_err();
+        assert_eq!(
+            "Expected Union<Integer, Float> but got String".to_string(),
+            err.to_string()
+        );
+    }
+
+    #[test]
+    fn test_contains_types() {
+        let expr = compile_expression("input1.contains(input2)", &["input1", "input2"]).unwrap();
+        let ty = expr
+            .run_types([Type::array_of_type(Type::Integer), Type::Integer])
+            .unwrap();
+        assert_eq!(Type::Boolean, ty);
+
+        let err = expr.run_types([Type::String, Type::Integer]).unwrap_err();
+        assert_eq!(
+            "Expected String but got Integer".to_string(),
+            err.to_string()
+        );
     }
 }
