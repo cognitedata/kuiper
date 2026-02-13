d1cce7ec8d664e8d641d1fb332abf0da35341fee
Einar Omang <einar.omang@cognite.com>
2026-02-13 13:11:50 +0100

    Type impls for to_object, reduce, select, and zip

--- kuiper_lang/src/expressions/functions/functors/reduce.rs
+++ kuiper_lang/src/expressions/functions/functors/reduce.rs
@@ -1,5 +1,6 @@
 use crate::expressions::functions::LambdaAcceptFunction;
 use crate::expressions::{Expression, ExpressionExecutionState, ResolveResult};
+use crate::types::Type;
 use crate::{BuildError, TransformError};
 use serde_json::Value;
 
@@ -30,6 +31,38 @@ impl<'a: 'c, 'c> Expression<'a, 'c> for ReduceFunction {
             )),
         }
     }
+
+    fn resolve_types(
+        &'a self,
+        state: &mut crate::types::TypeExecutionState<'c, '_>,
+    ) -> Result<crate::types::Type, crate::types::TypeError> {
+        let source = self.args[0].resolve_types(state)?;
+        let source_arr = source.try_as_array(&self.span)?;
+
+        let mut acc_type = self.args[2].resolve_types(state)?;
+
+        for elem in source_arr.all_elements() {
+            let res = self.args[1].call_types(state, &[&acc_type, elem])?;
+            acc_type = res;
+        }
+
+        // Check if we have converged to a single type, by seeing what happens if we try to pass the final result
+        // with end_dynamic to the lambda again. If we haven't, there's some form of dependency on the
+        // number of inputs in the array, and we can't be sure of the output type.
+        if let Some(end_dynamic) = source_arr.end_dynamic {
+            let Ok(res) = self.args[1].call_types(state, &[&acc_type, &end_dynamic]) else {
+                // There may only ever be one value of end_dynamic, but that's fine, the type is
+                // ok to use.
+                return Ok(acc_type);
+            };
+            // We don't converge, so the return type is indeterminable.
+            if res != acc_type {
+                return Ok(Type::Any);
+            }
+        }
+
+        Ok(acc_type)
+    }
 }
 
 impl LambdaAcceptFunction for ReduceFunction {
@@ -54,7 +87,7 @@ impl LambdaAcceptFunction for ReduceFunction {
 
 #[cfg(test)]
 mod tests {
-    use crate::compile_expression;
+    use crate::{compile_expression, types::Type};
 
     #[test]
     pub fn test_reduce_strings() {
@@ -75,5 +108,19 @@ mod tests {
 
         let val_arr = res.as_i64().unwrap();
         assert_eq!(val_arr, 10);
+    }
+
+    #[test]
+    fn test_reduce_types() {
+        let expr = compile_expression("input.reduce((acc, v) => acc + v, 0)", &["input"]).unwrap();
+        let res = expr
+            .run_types([Type::array_of_type(Type::Integer)])
+            .unwrap();
+        assert_eq!(res, Type::Integer);
+        let res = expr.run_types([Type::array_of_type(Type::Float)]).unwrap();
+        assert_eq!(res, Type::Float);
+
+        let r = expr.run_types([Type::Any]).unwrap();
+        assert_eq!(r, Type::number());
     }
 }
