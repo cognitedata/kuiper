a64964c69ebae778b3e47082c1a43685a4c159fd
Mathias Lohne <mathias.lohne@cognite.com>
2026-03-09 08:59:39 +0100

    Change how the `run` method works in the python bindings (#724)
    We previously only operated on strings, meaning users of the bindings
    needed to serialize/deserialize into JSON before running their
    expressions. This is often inconvenient.
    
    The new `PythonJson` type allows easy conversions between `serde_json`'s
    `Value` and `PyAny`, making it possible to make the `run` method take in
    native Python types as long as they are easily "JSONable".
    
    I also took the opportunity to clean up the signature of the run
    function, combining the `run, `run_with_multiple_inputs` and
    `run_limited` methods into a single `run`. It takes 0 or more inputs as
    args, and the operation limit as an optional kw arg.
    
    ```python
    expr = compile_expression(
        '{"message": concat(in2, " ", in1.key)}',
        ["in1", "in2"]
    )
    
    result = expr.run({"key": "world"}, "hello")
    print(result["message"])
    ```
    
    The old `run` which works on JSON strings is still available throuh
    `run_json`, but with the new signature style.
    
    ``` python
    result_str = expr.run_json('{"key": "world"}', '"hello"')
    ```
    
    This is a breaking change, and given we are in a `0.x` release, the next
    release should bump the minor version.
    
    This also drops support for Python 3.10 and 3.11.
    
    Python 3.10 is EOL later this year, 3.11 next year. I think it should be
    fine to drop support for both IMO. Even Debian is on 3.13 as it's system
    python by now.

--- kuiper_python/src/expressions.rs
+++ kuiper_python/src/expressions.rs
@@ -1,6 +1,13 @@
-use crate::exceptions::raise_kuiper_error;
+use crate::{
+    exceptions::raise_kuiper_error,
+    python_json::{ConversionError, PythonJson},
+};
 use kuiper_lang::ExpressionType;
-use pyo3::{pyclass, pymethods, PyResult};
+use pyo3::{
+    pyclass, pymethods,
+    types::{PyAnyMethods, PyTuple, PyTupleMethods},
+    Py, PyAny, PyResult, Python,
+};
 use serde_json::{from_str, Value};
 
 #[pyclass(module = "kuiper")]
@@ -33,50 +40,67 @@ impl KuiperExpression {
         };
         Ok(json)
     }
-
-    fn run_internal<'a>(&self, input: impl IntoIterator<Item = &'a str>) -> PyResult<String> {
-        let json = Self::get_expression_input(input)?;
-        match self.expression.run(json.iter()) {
-            Ok(result) => Ok(result.to_string()),
-            Err(transform_error) => Err(raise_kuiper_error(
-                "KuiperRuntimeError",
-                transform_error.to_string(),
-                transform_error.span().map(|s| s.start),
-                transform_error.span().map(|s| s.end),
-            )),
-        }
-    }
-
-    fn run_limited_internal<'a>(
-        &self,
-        input: impl IntoIterator<Item = &'a str>,
-        max_operations: i64,
-    ) -> PyResult<String> {
-        let json = Self::get_expression_input(input)?;
-        match self.expression.run_limited(json.iter(), max_operations) {
-            Ok(result) => Ok(result.to_string()),
-            Err(transform_error) => Err(raise_kuiper_error(
-                "KuiperRuntimeError",
-                transform_error.to_string(),
-                transform_error.span().map(|s| s.start),
-                transform_error.span().map(|s| s.end),
-            )),
-        }
-    }
 }
 
 #[pymethods]
 impl KuiperExpression {
-    fn run(&self, input: String) -> PyResult<String> {
-        self.run_internal([input.as_str()])
+    #[pyo3(signature = (*inputs, max_operations=None))]
+    fn run(&self, inputs: Py<PyTuple>, max_operations: Option<i64>) -> PyResult<Py<PyAny>> {
+        let inputs = Python::attach(|py| {
+            inputs
+                .bind(py)
+                .iter()
+                .map(|item| item.extract())
+                .collect::<PyResult<Vec<PythonJson>>>()
+        })?
+        .into_iter()
+        .map(|item| item.into_value())
+        .collect::<Result<Vec<_>, _>>()
+        .map_err(ConversionError::into_python_error)?;
+
+        let run_result = if let Some(op_limit) = max_operations {
+            self.expression.run_limited(inputs.iter(), op_limit)
+        } else {
+            self.expression.run(inputs.iter())
+        };
+
+        let run_result = run_result
+            .map_err(|transform_error| {
+                raise_kuiper_error(
+                    "KuiperRuntimeError",
+                    transform_error.to_string(),
+                    transform_error.span().map(|s| s.start),
+                    transform_error.span().map(|s| s.end),
+                )
+            })?
+            .into_owned();
+
+        Python::attach(|py| {
+            Ok(PythonJson::from_value(run_result)
+                .map_err(ConversionError::into_python_error)?
+                .into_python(py))
+        })
     }
 
-    fn run_multiple_inputs(&self, inputs: Vec<String>) -> PyResult<String> {
-        self.run_internal(inputs.iter().map(|s| s.as_str()))
-    }
+    #[pyo3(signature = (*inputs, max_operations=None))]
+    fn run_json(&self, inputs: Vec<String>, max_operations: Option<i64>) -> PyResult<String> {
+        let json = Self::get_expression_input(inputs.iter().map(String::as_str))?;
 
-    fn run_limited(&self, inputs: Vec<String>, max_operations: i64) -> PyResult<String> {
-        self.run_limited_internal(inputs.iter().map(|s| s.as_str()), max_operations)
+        let run_result = if let Some(op_limit) = max_operations {
+            self.expression.run_limited(json.iter(), op_limit)
+        } else {
+            self.expression.run(json.iter())
+        };
+
+        match run_result {
+            Ok(result) => Ok(result.to_string()),
+            Err(transform_error) => Err(raise_kuiper_error(
+                "KuiperRuntimeError",
+                transform_error.to_string(),
+                transform_error.span().map(|s| s.start),
+                transform_error.span().map(|s| s.end),
+            )),
+        }
     }
 
     fn __str__(&self) -> String {
